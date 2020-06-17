50ec152a62cc513d48e0c50d4d1a8f45ae8e738a
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-17 15:07:36 -0400

    append tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// object from initial-tweets.json 
+  // temp hard coded, but later will be loading from Express server via AJAX GET req
+
+$(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+// function createTweetElement that takes in a tweet object
+  // and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
+  const createTweetElement = function(tweet) {
+    const htmlTweet = `
+    <article class="tweets">
+      <header>
+        <h4 class="name">${tweet.user.name}</h4>
+        <img src="${tweet.user.avatars}"
+        <h4 class="account">${tweet.user.handle}</h4>
+      </header>
+      <div class="tweet">
+        <p>${tweet.content.text}</p>
+      </div>
+      <footer>
+        <p>${tweet.created_at}</p>
+        <div>
+          <i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+        </div>
+        </footer>
+      </article>
+  `
+    return htmlTweet;
+  }
+  // function takes in array of tweet objects and appends each one to the #tweets-container
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $('#tweet-list').append(createTweetElement(tweet));
+    }
+  }
+  renderTweets(data);
+})
