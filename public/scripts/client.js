e2fff76db6965550b59f3ac2930e2dee6fb965b2
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-17 18:48:26 -0400

    ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,29 +10,29 @@
 $(document).ready(function() {
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
 // function createTweetElement that takes in a tweet object
   // and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
@@ -68,26 +68,38 @@ $(document).ready(function() {
       $('#tweet-list').append(createTweetElement(tweet));
     }
   }
-  renderTweets(data);
+  // renderTweets(data);
 
   // AJAX POST request 
   $('form').on('submit', function(event) {
     event.preventDefault();
-    console.log("Registered");
+    //console.log("Registered");
     $.ajax({
       url: '/tweets',
       method: 'POST',
       data: $(this).serialize()
     }).then(function() {
       console.log('success!', this)
-      $('.tweet-container').empty();
-      renderTweets(data);
+      // $('.tweet-container').empty();
+      // renderTweets(data);
     })
   });
 
-  // GET request that fetches tweets from /tweets
+  // AJAX GET request that fetches tweets from /tweets
   const loadTweets = function() {
-
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+      // console.log("registered");
+      $.ajax({
+        url: '/tweets',
+        method: 'GET',
+        dataType: 'JSON'
+      }).then(function(response) {
+        // console.log("response", response)
+        $('.tweet-container').empty();
+        renderTweets(response);
+      })
+    })
   }
-
+  loadTweets()
 });
